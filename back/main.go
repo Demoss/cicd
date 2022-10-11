1149ccd0d14171cd5ad2c4f7bb438ba0cfe6fcf5
Demoss <intetstellar2@gmail.com>
2022-10-11 12:28:00 +0300

    change back

--- back/main.go
+++ back/main.go
@@ -15,7 +15,7 @@ func main() {
 	corsConfig.AllowAllOrigins = true
 	router := gin.Default()
 	router.Use(cors.New(corsConfig))
-	hello := Greetings{Hello: "Hello"}
+	hello := Greetings{Hello: "Oleksiy Hello"}
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": hello.Hello})
