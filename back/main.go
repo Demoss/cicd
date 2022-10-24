5d3d9b9ee9de29e53b37a9239599c034a8d2fa7b
Demoss <intetstellar2@gmail.com>
2022-10-24 10:21:44 +0300

    check something

--- back/main.go
+++ back/main.go
@@ -16,7 +16,7 @@ func main() {
 	corsConfig.AllowAllOrigins = true
 	router := gin.Default()
 	router.Use(cors.New(corsConfig))
-	hello := Greetings{Hello: os.Getenv("test")}
+	hello := Greetings{Hello: os.Getenv("test2")}
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": hello.Hello})
